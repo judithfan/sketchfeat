95de4bba13f72e7d35444f104a8d1e135e03d51c
Rachel S. Lee <rslee@tigergpu.princeton.edu>
2017-09-01 17:03:19 -0400

    batched style for vgg.

--- rachel_vgg16_small.py
+++ rachel_vgg16_small.py
@@ -24,7 +24,7 @@ NUM_VIEWS = 40
 # to upload multiple images
 
 cars = ['limoToSUV_10','limoToSUV_99','smartToSedan_10','smartToSedan_99'];
-furnitures = ['bedChair_1', 'bedChair_100', 'benchBed_1', 'benchBed_100']
+furnitures = ['bedChair_1', 'bedChair_100', 'tableBench_1', 'tableBench_100']
 
 batch = np.empty((0, VGG_SIZE_X, VGG_SIZE_Y, VGG_SIZE_Z), float)
 for obj in cars + furnitures:
@@ -34,11 +34,9 @@ for obj in cars + furnitures:
         img = utils.load_image(imgloc)
         img = img.reshape(1, VGG_SIZE_X, VGG_SIZE_Y, VGG_SIZE_Z)
         batch = np.concatenate((batch, img))
-
-        
  # smaller batch for testing first
 print batch.shape[0]
-batch_mini = batch[:2,:,:,:]
+batch_mini = batch[:160,:,:,:]
 print batch_mini.shape[0]
 
 
@@ -54,15 +52,42 @@ with tf.device('/gpu:0'):
         with tf.name_scope("content_vgg"):
             vgg.build(image)
 	
-        act_wanted = [vgg.pool1, vgg.pool2, vgg.prob]
-        act = sess.run(act_wanted, feed_dict=feed_dict)
+        act_wanted = [vgg.pool1, vgg.pool2, vgg.pool3, vgg.pool4, vgg.pool5, vgg.fc6, vgg.fc7, vgg.prob]
+	act = sess.run(act_wanted, feed_dict=feed_dict)
 
-        for i in xrange(0, batch_mini.shape[0]):
-            utils.print_prob(act[2][i], './synset.txt')
          
 print "completed running the VGG on " + str(batch.shape[0])
 print "now saving...."
 pickle.dump(act, open( "/tigress/rslee/activations.p", "wb" ) )
 print "saved vgg"
-       
-       
+
+# hacking this for now. using two batches instead of one because of memory issues. 
+ # smaller batch for testing first
+print batch.shape[0]
+batch_mini = batch[160:,:,:,:]
+print batch_mini.shape[0]
+
+
+
+
+with tf.device('/gpu:0'):
+    with tf.Session() as sess:
+        image = tf.placeholder("float", [batch_mini.shape[0], VGG_SIZE_X, VGG_SIZE_Y, VGG_SIZE_Z])
+
+        feed_dict = {image: batch_mini}
+
+        vgg = vgg16.Vgg16()
+        with tf.name_scope("content_vgg"):
+            vgg.build(image)
+
+        act_wanted = [vgg.pool1, vgg.pool2, vgg.pool3, vgg.pool4, vgg.pool5, vgg.fc6, vgg.fc7, vgg.prob]
+        act = sess.run(act_wanted, feed_dict=feed_dict)
+
+
+print "completed running the VGG on " + str(batch.shape[0])
+print "now saving...."
+pickle.dump(act, open( "/tigress/rslee/activations2.p", "wb" ) )
+print "saved vgg"
+
+# for i in xrange(0, batch_mini.shape[0]):
+#    utils.print_prob(act[7][i], './synset.txt')       
